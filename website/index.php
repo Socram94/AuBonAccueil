b111576e28208f5aaeb64ec8299b09bf69ae698b
Alexandre Marcos <alexandremarcos94100@gmail.com>
2023-03-29 19:25:40 +0200

    Create main page

---
+++ website/index.php
@@ -0,0 +1,26 @@
+<?php
+echo "hello world";
+?>
+<html>
+<head>
+<meta charset="utf-8">
+    <title>Au Bon Accueil</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<!--
+    include le header
+-->
+<?php
+    include 'Generic/header.php';
+?>
+<body>
+    <!-- bienvenue au bon accueil 
+        Bouton inscripton/Connexion
+-->
+
+</body>
+<!--
+    Include le footer ici
+-->
+</html>
